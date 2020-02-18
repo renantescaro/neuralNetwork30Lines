ac5a244a13835b444d11932d37e920e5549726a6
Renan Tescaro <renantescaro@yahoo.com.br>
2020-02-18 13:47:40 -0300

    subindo

---
+++ redeNeural.js
@@ -0,0 +1,55 @@
+
+// Fonte -> https://medium.com/machina-sapiens/como-criar-uma-rede-neural-em-javascript-com-apenas-30-linhas-de-c%C3%B3digo-63273d83dc04
+
+const { Layer, Network } = window.synaptic
+
+// Quantidade de neuronios por camada
+var camadaEntrada = new Layer(2)
+var camadaOculta = new Layer(3)
+var camadaSaida = new Layer(1)
+
+// conecta as camadas formando a rede
+camadaEntrada.project(camadaOculta)
+camadaOculta.project(camadaSaida)
+
+var rede = new Network({
+  input: camadaEntrada,
+  hidden: [camadaOculta],
+  output: camadaSaida
+})
+
+// constante para ajustes de pesos
+var taxaAprendizagem = .3
+
+/*  propagação:
+
+Fonte-> http://deeplearningbook.com.br/algoritmo-backpropagation-parte-2-treinamento-de-redes-neurais/
+
+1. O passo para frente (forward pass), onde nossas entradas são passadas através da rede
+   e as previsões de saída obtidas (essa etapa também é conhecida como fase de propagação).
+
+2. O passo para trás (backward pass), onde calculamos o gradiente da função de perda na camada final
+   (ou seja, camada de previsão) da rede e usamos esse gradiente
+   para aplicar recursivamente a regra da cadeia (chain rule) para atualizar
+   os pesos em nossa rede (etapa também conhecida como fase de atualização de pesos ou retro-propagação).
+*/
+
+for (var i = 0; i < 20000; i++) {
+
+  rede.activate([0,0]) // 0,0 => 0
+  rede.propagate(taxaAprendizagem, [0])
+  rede.activate([0,1]) // 0,1 => 1
+  rede.propagate(taxaAprendizagem, [1])
+  rede.activate([1,0]) // 1,0 => 1
+  rede.propagate(taxaAprendizagem, [1])
+  rede.activate([1,1]) // 1,1 => 0
+  rede.propagate(taxaAprendizagem, [0])
+}
+
+var dvSaidas = document.getElementById('dvSaidas')
+
+// Saidas
+console.log( Math.round(rede.activate([0,0])) )
+console.log( Math.round(rede.activate([0,1])) )
+console.log( Math.round(rede.activate([1,0])) )
+console.log( Math.round(rede.activate([1,1])) )
